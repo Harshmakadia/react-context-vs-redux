76f776575073eba489a4ca3cc0cff4715dd7f60f
Harsh <harsh.makadia@marutitech.com>
2019-02-24 18:13:33 +0530

    Updates

---
+++ precache-manifest.de1d0f93ade2fd437f34dbc189f39eab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96fc44c03d69a5159f78",
+    "url": "/react-context-vs-redux/static/js/runtime~main.96fc44c0.js"
+  },
+  {
+    "revision": "56bedbd94425dbd8fb5a",
+    "url": "/react-context-vs-redux/static/js/main.56bedbd9.chunk.js"
+  },
+  {
+    "revision": "f34c098d76620e1c3b7d",
+    "url": "/react-context-vs-redux/static/js/1.f34c098d.chunk.js"
+  },
+  {
+    "revision": "56bedbd94425dbd8fb5a",
+    "url": "/react-context-vs-redux/static/css/main.fccc3846.chunk.css"
+  },
+  {
+    "revision": "9cfef390d12d1c50dcc99b1827f4f4ef",
+    "url": "/react-context-vs-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f125509ba74738b7c94491d6bdeec0c3.js"
+  "/react-context-vs-redux/precache-manifest.de1d0f93ade2fd437f34dbc189f39eab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/react-context-vs-redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
